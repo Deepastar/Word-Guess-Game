c52cc3e588c471da8d1a1af8568702dabf0fa7de
Deepa Ravichandran <deepa5starbee@gmail.com>
2018-07-19 01:25:06 -0400

    Changes to javasript for game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,17 @@ var winsCount = 0;
 var lossCount = 0;
 var remGuesses = 10;
 var guessedLetters = [];
+var wordToGuess = getNewWordToGuess();
+var dashArray = [];
+var winFlag = false;
 
 function initialize() {
-    
-    var wordToGuess = getNewWordToGuess();
-    
+    dashArray = [];
+    guessedLetters = [];
+    winFlag = false;
+    document.getElementById("winStatus").style = "display: none";
     document.getElementById("winsCount").innerHTML = winsCount;
     document.getElementById("lossCount").innerHTML = lossCount;
-    var dashArray = [];
 
     for(var i = 0; i < wordToGuess.length; i++){
         dashArray.push("-");
@@ -26,8 +29,51 @@ function getNewWordToGuess(){
 
 document.onkeydown = function(event){
     var letter = event.key;
-    if(!guessedLetters.includes(letter, 0)){
-        guessedLetters.push(letter);
+
+    if(winFlag){
+        initialize();
+        return;
     }
-    document.getElementById("charGuessed").innerHTML = guessedLetters;
+        
+
+    var indexArray = findCharIndex(letter);
+
+    if(indexArray.length > 0){
+        for(var i = 0; i < indexArray.length; i++){
+            dashArray[indexArray[i]] = letter;
+        }
+        document.getElementById("currentWord").innerHTML = dashArray.join(" ");
+    } else{
+        if(!guessedLetters.includes(letter, 0)){
+            guessedLetters.push(letter);
+            remGuesses = remGuesses - 1;
+            document.getElementById("charGuessed").innerHTML = guessedLetters;
+            document.getElementById("remGuess").innerHTML = remGuesses;
+        } 
+    }
+
+    if(didWeWin()){
+        winsCount = winsCount + 1;
+        document.getElementById("winStatus").innerHTML = "YOU WIN";
+        document.getElementById("winStatus").style = "display: block";
+        winFlag = true;
+    }
+}
+
+function findCharIndex(letter){
+    var indexArray = [];
+    for(var i = 0; i < wordToGuess.length; i++){
+        if(wordToGuess.charAt(i) == letter){
+            indexArray.push(i);
+        }
+    }
+
+    return indexArray;
+}
+
+function didWeWin(){
+    if(wordToGuess == dashArray.join(""))
+        return true;
+    else
+        return false;
 }
